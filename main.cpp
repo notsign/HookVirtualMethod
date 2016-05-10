da36bc2ddbc08c6a44a3bdecd638c13f56c12c81
notsign <notsign@outlook.com>
2016-05-10 10:54:15 -0400

    Added dummy protection variable
    Added a dummy protection variable to ensure that it isn't writing the old protection to oldProtection before writing the protection. Probably not an issue but I just wanted to be sure.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ void HookVirtualMethod(void* pInstance, int index, void* pNewFunc) {
 	void** pTable = *(void***)pInstance; // The vmt ptr is the first member
 	void** ppFunc = pTable + index;
 
-	DWORD oldProtection;
+	DWORD oldProtection, dummyProtection;
 
 	// Make sure we have readwrite access to the memory region
 	VirtualProtect(ppFunc, sizeof(ppFunc), PAGE_EXECUTE_READWRITE, &oldProtection);
@@ -13,7 +13,7 @@ void HookVirtualMethod(void* pInstance, int index, void* pNewFunc) {
 	*ppFunc = pNewFunc;
 
 	// Return the original mem access afterwards
-	VirtualProtect(ppFunc, sizeof(ppFunc), oldProtection, &oldProtection);
+	VirtualProtect(ppFunc, sizeof(ppFunc), oldProtection, &dummyProtection);
 }
 
 void* GetVirtualMethod(void* pInstance, int index) {
